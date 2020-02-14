3c1a14159e93c6d10143c595d26760b3227384e8
ciscou <francismpp@gmail.com>
2020-02-14 23:57:39 +0100

    blueimp gallery

--- source/javascripts/app.js
+++ source/javascripts/app.js
@@ -3,4 +3,13 @@
     e.preventDefault();
     $("#overlay-nav-menu").toggleClass("is-open");
   });
+
+  $photos = $(".photos a.photo")
+
+  $(".photos a.photo").click(function(e) {
+    e.preventDefault();
+    e.stopPropagation();
+
+    blueimp.Gallery($photos);
+  });
 })(jQuery);
