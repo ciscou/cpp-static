fbd945d56ed5c541c75b3f9117fb4fbb4a5db919
ciscou <francismpp@gmail.com>
2020-02-15 15:49:38 +0100

    actual description

--- source/javascripts/app.js
+++ source/javascripts/app.js
@@ -9,8 +9,36 @@
   $(".photos a.photo").click(function(e) {
     var target = e.target || e.srcElement;
     var link = target.src ? target.parentNode : target;
-    var options = { index: link, event: e };
+    var options = {
+      index: link,
+      event: e,
+      onopen: function() {
+        // console.log('open');
+      },
+      onopened: function() {
+        // console.log('opened');
+      },
+      onslide: function(index, slide) {
+        // console.log('slide', index, slide);
+        var description = $(this.list[index]).data("description");
+        $("#blueimp-gallery .description").html(description);
+      },
+      onslideend: function(index, slide) {
+        // console.log('slideend', index, slide);
+      },
+      onslidecomplete: function(index, slide) {
+        // console.log('slidecomplete', index, slide);
+      },
+      onclose: function() {
+        // console.log('close');
+      },
+      onclosed: function() {
+        // console.log('closed');
+      }
+    };
 
     blueimp.Gallery($photos, options);
   });
+
+  $('#blueimp-gallery')
 })(jQuery);
