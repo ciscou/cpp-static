3e502049c987426c28f57a00ee536bb261cdaf9c
ciscou <francismpp@gmail.com>
2020-02-15 14:33:09 +0100

    open selected picture

--- source/javascripts/app.js
+++ source/javascripts/app.js
@@ -7,9 +7,10 @@
   $photos = $(".photos a.photo")
 
   $(".photos a.photo").click(function(e) {
-    e.preventDefault();
-    e.stopPropagation();
+    var target = e.target || e.srcElement;
+    var link = target.src ? target.parentNode : target;
+    var options = { index: link, event: e };
 
-    blueimp.Gallery($photos);
+    blueimp.Gallery($photos, options);
   });
 })(jQuery);
